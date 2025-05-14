f0491fbc59205354cc52acf95addfffd03b66da1
wlhd <112258449+wlhd7@users.noreply.github.com>
2025-05-14 14:44:16 +0800

    Update PatternFill().py

--- API/openpyxl.styles/PatternFill().py
+++ API/openpyxl.styles/PatternFill().py
@@ -5,9 +5,13 @@ wb = Workbook()
 ws = wb.active
 
 ws['A2'].fill = PatternFill(
-  start_color = 'FFFF00', 
-  end_color = 'FFFF00',
-  fill_type = 'solid')
+  fgColor='FFFFFF00'
+  start_color='FFFF00', 
+  end_color='FFFF00',
+  fill_type='solid')
+
+if ws['A2'].fill.fgColor.rgb == 'FFFFFF00': return('fgColor')
+if ws['A2'].fill.start_color == 'FFFFFF00': return('start_color')
 
 wb.save('new.xlsx')
 
